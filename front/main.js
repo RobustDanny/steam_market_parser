6d6444f1d12b9da52c06bba8adc9a79c9ee9e255
RobustDanny <deeabusal@gmail.com>
2026-02-01 13:04:18 -0500

    Refactor JavaScript imports in main.js for improved modularity
    - Updated the import structure in `main.js` to include new components such as `main_side_filters.js`, `main_websocket_handler.js`, and `main_media_panel.js`.
    - Removed outdated imports for better organization and clarity.
    - Enhanced the overall modularity of the codebase by integrating new profile-related components.

--- front/main.js
+++ front/main.js
@@ -1,20 +1,19 @@
 
 import "./js/misc_shared_fns.js";
-import "./js/main_head_panel.js";
 
-import "./js/profile_ad.js";
-import "./js/profile_settings.js";
+import "./js/main_head_panel.js";
 import "./js/main_side_filters.js";
+import "./js/main_websocket_handler.js";
+import "./js/main_media_panel.js";
 
 import "./js/profile_ad_user_inventory.js";
+import "./js/profile_store_queue.js";
+import "./js/profile_ad.js";
+import "./js/profile_settings.js";
+
 import "./js/store.js";
 import "./js/store_menu.js";
-import "./js/profile_store_queue.js";
-
-import "./js/main_websocket_handler.js";
 import "./js/store_websocket.js";
-
-import "./js/main_media_panel.js";
 
 //Widget
 import "./js/troopack_widget.js";
